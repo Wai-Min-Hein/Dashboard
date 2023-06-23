d0faf1b0e91bafef5422793801317020fe55e57d
Wai-Min-Hein <wai06276@gmail.com>
2023-06-23 17:49:16 +0630

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Ecommerce from "./Pages/Ecommerce";
 import SideBar from "./Pages/SideBar";
-import TopBar from "./Pages/TopBar";
 
 const App = () => {
   return (
     <div className="flex items-start justify-start h-screen">
       <SideBar />
-      <div className="flex-1">
-        <TopBar/>
+      
 
       <Routes>
         <Route path="/" element={<Ecommerce />} />
       </Routes>
 
-      </div>
     </div>
   );
 };
